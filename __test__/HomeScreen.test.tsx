0683d35937edee4789f4b6ceea1e0ccd7d6289e1
Vinh <phuvi301@gmail.com>
2026-04-26 19:35:31 +0700

    move another test to __test__ folder

--- __test__/HomeScreen.test.tsx
+++ __test__/HomeScreen.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "@jest/globals";
+// import { describe, expect, it } from "@jest/globals";
 import { fireEvent, render } from "@testing-library/react-native";
 import React from "react";
 
